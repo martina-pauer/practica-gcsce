f5a9862b52dfdf28f60354170092a48537d994a2
Martina Pauer <martinapauer38@gmail.com>
2025-02-01 14:31:21 -0300

    Mostrar caracteres en vertical de nombre de ciudad
    practica de for to strings

---
+++ cartelizar.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''
+    Muestra en vertical en parentesis si no es espacio en blanco
+    el nombre de una ciudad (sirve de boceto para los carteles)
+'''
+# Obtengo nombre de ciudad
+ciudad = input('Ingrese nombre de ciudad: ')
+# Comienzo a mostrar
+print('\n')
+# Recorro cada uno de los caracteres del nombre
+for caracter in ciudad:
+    if (caracter != ' ') and (caracter != ''):
+    # Muestro si no es caracter nulo o en blanco
+        print('\t(', caracter, ')\t')
+    else:
+        # Nueva linea cuando no hay letras que mostrar
+        print('\n')
+# Termino de mostrar
+print('\n')
